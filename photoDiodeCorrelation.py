d512390d2d041a6038bac85040a7c13b92eda214
Andrew O'Harney <andrew0harney@users.noreply.github.com>
2014-03-17 11:54:49 -0400

    Update photoDiodeCorrelation.py

--- photoDiodeCorrelation.py
+++ photoDiodeCorrelation.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from Grid import Grid
-from Grid import show_events_on_chan,photodiode_signal
+from SignalManager import SignalManager
+from SignalManager import show_events_on_chan,photodiode_signal
 
 
 #Script to check that events are aligned with some independent measurement
@@ -14,18 +14,17 @@ from Grid import show_events_on_chan,photodiode_signal
 data_path = #Path to data
 log_path =  #Path to log
 #   
-grid = NewBrain(data_path,log_file=log_path)
+grid = SignalManager(data_path,log_file=log_path)
 #
 print 'Generating'
 wanted_events = [2,3]
 
-diodeSignal = photodiode_signal(grid)
-#event_colours = ['g','b','y']
+indpntSignal = photodiode_signal(grid)
 #show_events_on_chan(grid,'C127',wanted_events,colours=event_colours)
 print 'Plotting'
 a = grid.data(columns=['C127'])
-plt.plot(diodeSignal[2000000:]*0.02)
+plt.plot(indpntSignal)
 plt.hold(True)
-plt.plot(a[2000000:])
+plt.plot(a)
 plt.plot()
 plt.show()
