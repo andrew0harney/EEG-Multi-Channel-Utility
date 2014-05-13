35c77c7eb7eefeeb8678cf781b2f32b410dd5aab
Andrew O'Harney <andrew0harney@users.noreply.github.com>
2014-05-13 12:16:46 -0400

    Update SignalManager.py
    Comments and some small fixes

--- SignalManager.py
+++ SignalManager.py
@@ -6,15 +6,20 @@ import gc
 import logging
 import mne
 
-logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('__SignalManager__')
+  
+"""Utility that acts as a wrapper around the Python Pandas package, which is useful for handling time series data efficiently.
+   In particular, the management tool helps to sanity check, align and utilise experimentally relevant epochs in multi channel data"""
+
+__author__ =  'Andrew O\Harney'
 
 class SignalManager:
-    #This class manages signals in edf,fif,or hd5 format (note all files are converted to hd5)
-    #It makes extensive use of pandas to represent time series and events logs
-    #requirements - pandas, numpy,matplotlib
-    #             - mne (utility functions)
-    #             - matplotlib (utility functions/plotting)
+    """This class manages signals in edf,fif,or hd5 format (note all files are converted to hd5)
+    It makes extensive use of pandas to represent time series and events logs
+    
+    Requirements - pandas,numpy,matplotlib
+                 - mne (utility functions)
+                 - matplotlib (utility functions/plotting)"""
     
     
     ############Member Variables################
@@ -31,13 +36,15 @@ class SignalManager:
     
     
     def __init__(self,base_file_name=None,log_file=None,offsets=None,new_log_out=False,eventsKey=None):
-        #Initialiser takes the path of the signal data
+        """Initialiser takes the path of the signal data
         #Can also set the event matrix or generate it from a log path
-        #
-        #Requires : base_file_name - the path and name of the signal data with no file extension
-        #Optional : log_file - path to log file of events
-        #         : offsets : if a log file has been provided, startimes is a path to a file containing appropriate offets for each block of events (if required - often useful for alignment)
-        #         : new_log_out - if Offsets is specified then new_log_out is a Boolean value if the corrected file is to be output
+        
+        Keyword arguments:
+        base_file_name=None -- The path and name of the signal data with no file extension
+        log_file=None -- Path to log file of events
+        offsets=None -- If a log file has been provided, startimes is a path to a file containing appropriate offets for each block of events (if required - often useful for alignment)
+        new_log_out=None -- If offsets is specified then new_log_out is a Boolean value if the corrected file is to be output
+        eventsKey=None -- If special event codes are required (i.e not from the csv file)"""
         
         self.__base_file_name = base_file_name
         logger.debug('Using file : %s'%(base_file_name))
@@ -55,10 +62,9 @@ class SignalManager:
     
 
     def __load_data__(self):
-        #Attempts to load data from .hd5
-        #If .hd5 file does not exist, it will try to convert to it
-        #
-        #try:
+        """Attempts to load data from .hd5
+        If .hd5 file does not exist, it will try to convert to it"""
+
         if self.base_file_name() is None:
             raise Exception('Data was not specified')
         elif self.__check_for_files__('hd5'): #HD5 is the basis for pytables
@@ -76,11 +82,13 @@ class SignalManager:
         #except Exception as e: raise e
 
     def __check_for_files__(self, ftype):
-        #Returns a list of files in the local Directory containing the base_file_name
+        """Returns a list of files in the local Directory containing the file type
+        Keyword Arguments:
+        ftype -- Type of file to check for"""
         return glob.glob(self.__base_file_name + '.' + ftype)
     
     def __open_hdf5__(self):
-        #Tries to open hd5 file
+        """Tries to open hd5 file"""
         try:
             self.__signals = pd.HDFStore(self.__base_file_name+'.hd5')
         except:
@@ -88,7 +96,8 @@ class SignalManager:
             raise Exception('Could not open hd5 file')
 
     def __edf_2_fif__(self):
-        #Tries to convert edf to fif
+        """"Tries to convert edf to fif"""
+        
         sysString = 'mne_edf2fiff --edf '+self.base_file_name()+'.edf --fif ' + self.base_file_name()+'.fif'
         logger.info('System Command : %s'%(sysString))
         try:
@@ -102,7 +111,6 @@ class SignalManager:
         #NEED TO THINK ABOUT THIS ONE
         pass
         #Will remove parts of the signal not important to the experiment
-        
         em = self.event_matrix()
         em = em[em['event.code'].isin(importantEventCodes)]
         exptimes = self.eventsTimes(em)
@@ -111,7 +119,7 @@ class SignalManager:
     
        
     def __fif_2_hdf5__(self):
-        #Tries to convert .fif file to .hd5 format
+        """Tries to convert .fif file to .hd5 format"""
         
         #Get data from the raw .fif file
         try:
@@ -131,9 +139,9 @@ class SignalManager:
         self.__open_hdf5__()
 
     def __create_events_matrix__(self):
-        #Creates a Dataframe with index=data timestamps times, columns=signal channels
+        """Creates a Dataframe with index=data timestamps times, columns=signal channels"""
         logger.info( "Generating event matrix")
-        events = pd.read_csv(self.__log_file,delimiter='\t')
+        events = pd.read_csv(self.__log_file,delimiter=r'\t|,')
         logger.debug( 'Found columns:'+str(events.columns))
         self.__signals['event_matrix'] = events
         self.__flushSignals__()
@@ -142,8 +150,8 @@ class SignalManager:
           
 
     def __find_blocks__(self):
-        #Finds the on and off times of blocks
-        #Note: Blocks are defined as starting at event types blockStart(event id 1)
+        """Finds the on and off times of blocks
+        Note: Blocks are defined as starting at event types blockStart(event id 1)"""
         
         logger.info( "Finding blocks")
         logger.debug( '\tCalculating block indices')
@@ -169,27 +177,30 @@ class SignalManager:
         self.__flushSignals__()
     
     def __flushSignals__(self):
-        #Forces a write to the hdf file
+        """"Forces a write to the hdf file"""
         self.__signals.flush()
            
     ###############Public Methods#################################
 
     def set_eventsKey(self,eventsKey):
-        #Set a dictionary containing event code descriptions
+        """Set a dictionary containing event code descriptions
+        Keyword Arguments:
+        eventsKey -- Dictionary containing event codes and label names"""
         self.__eventskey = eventsKey
     
     def eventsKey(self):
-        #Get events key
+        """Get events key"""
         return self.__eventskey    
     
 
     @staticmethod
     def save_hdf(data,times,cnames,fs,base_file_name):
-        #Takes raw data and saves to HD5
+        """Takes raw data and saves to HD5
+        Keyword arguments:
         #data - raw signal data
         #cnames - channel names
         #base_file_name - base file name
-        #fs - sample rate of data
+        #fs - sample rate of data"""
         
         (x,y)= data.shape
         #Store in hd5(pytables) format
@@ -212,11 +223,11 @@ class SignalManager:
     
 
     def add_channel(self,sig,name):
-        #Adds a channel to the hd5 file
-        #params sig - the raw signal to be added
-        #       name - name of the new channel
-        #       addToWd - add this channel to the working data
-        #       mean - 'global' or 'local' (i.e use all channels or only those in the working data set)
+        """Adds a channel to the hd5 file
+        
+        Keyword Arguments:
+        sig -- the raw signal to be added
+        name -- name of the new channel"""
         
         if name not in self.channels():
             newData = self.data()
@@ -228,7 +239,9 @@ class SignalManager:
             logger.info( 'Channel with that name already exists')
              
     def remove_channel(self,chan):
-        #Removes channel chan from the persistent .hd5 file
+        """Removes channel chan from the persistent .hd5 file
+        Keyword Arguments:
+        chan -- Channel to be removed"""
         
         #Try and remove the specified channel
         try:
@@ -252,35 +265,39 @@ class SignalManager:
  
     #################Public Methods#########################   
     def blocks(self):
-        #Return the on and off times of blocks
+        """Return the on and off times of blocks"""
         return self.__signals['blocks']
     
     
     def base_file_name(self):
-        #Return the base file path
+        """Return the base file path"""
         return self.__base_file_name
     
     
     def log_file_name(self):
-        #Returns the log path of the psychopy file in use (i.e the file the event_matrix was generated from)
+        """"Returns the log path of the psychopy file in use (i.e the file the event_matrix was generated from)"""
         return self.__log_file
     
     def event_matrix(self,types=None):
-        #Returns the event matrix
+        """Returns the event matrix
+        
+        Keyword Arguments:
+        types -- Event types (codes to return) - Still to be implemented
+        """
         return self.__signals['event_matrix']
 
         
     def calc_mean(self,channels):
-        #Return the mean channel (i.e the mean power across all channels for a given time point)
-        #channels - channels to calculate the mean over
+        """Return the mean channel (i.e the mean power across all channels for a given time point)
+        channels -- Channels to calculate the mean over"""
     
         return pd.Series(self.data(channels=channels).mean(axis=1),index=self.times())
     
     
     def set_mean(self,meanCalcChans=None,meanApplyChans=None):
-        #Remove a mean value from channels
-        #meanCalcChans : channels to calculate the mean from - mean will be applied to these channels unless meanApplyChans is specified
-        #meanApplyChans : channels to apply the mean to  (default is meanCalcChans)
+        """Remove a mean value from channels
+        meanCalcChans=None : channels to calculate the mean from - mean will be applied to these channels unless meanApplyChans is specified
+        meanApplyChans=None : channels to apply the mean to  (default is meanCalcChans)"""
         
         if meanCalcChans is not None:
             logger.info( 'Calculating mean')
@@ -295,8 +312,9 @@ class SignalManager:
         
 
     def data(self,channels=None):
-        #Efficiently get data chunks from disk by supplying a column list (Note:data must be in table format)
-        #Optional: columns - the channels to pull from the data
+        """Efficiently get data chunks from disk by supplying a column list (Note:data must be in table format)
+        Keyword Arguments: 
+        channels=None -- The channels to pull from the data (This should be done efficiently but needs to be reviewed)"""
         if channels is not None:
             try:
                 #Efficient read from disk (no need to load all data in memory) if data is in table format
@@ -311,32 +329,38 @@ class SignalManager:
         return d
     
     def times(self):
-        #Returns the timestamps of samples
+        """Returns the timestamps of samples"""
+        
         return self.__signals['times']
     
     def channels(self):
-        #Returns all channel names
+        """Returns all channel names"""
+        
         return self.__signals['channels']
 
     def wd(self,channels=None):
-        #Return the current working data
+        """Return the current working data
+        Keyword arguments:
+        channels=None -- Channels to pull from working data"""
         if self.__wd is not None:
             return self.__wd[self.wc() if channels is None else channels]
         else:
             logger.debug( "No working data was set")
        
     def fs(self):
-        #Return the sample rate of the signal
+        """Return the sample rate of the signal"""
+        
         return self.__signals['fs'][0]
     
 
     def correct_event_times(self,offsets,new_log_out=False):
-            #Correct the event matrix to include the appropriate block offsets
-            #Required : Offsets - a Pandas Dataframe or Series with ['time'] offsets for each block
-            #Optional : new_log_out - Boolean value if the corrected file is to be output
+            """Correct the event matrix to include the appropriate block offsets
+            #Keyword Arguments
+            Offsets -- A Pandas Dataframe or Series with ['time'] offsets for each block
+            new_log_out=None -- Boolean value if the corrected file is to be output"""
     
             logger.info( 'Correcting times in log file')
-            offsets = pd.read_csv(offsets)
+            offsets = pd.read_csv(offsets,sep='\t')
             blocks = self.blocks()
             startTimes = blocks['pulse.on']
             
@@ -363,10 +387,13 @@ class SignalManager:
                 self.__log_file = self.__base_file_name+'_corrected_log.csv'
             
     def set_log_file(self,log,offsets=None,new_log_out=False):
-        #Sets the psychopy log file
-        #Required: log - path to log file
-        #Optional: offsets - path to file contains offsets of block times
-        #        : new_log_out - Boolean value if the corrected file is to be output
+        """Sets the psychopy log file
+        
+        Keyword Arguments: 
+        log -- path to log file
+        offsets=None -- path to file contains offsets of block times
+        new_log_out=False -- Boolean value if the corrected file is to be output"""
+        
         logger.info( 'Saving log file')
         self.__log_file = log
         self.__create_events_matrix__()
@@ -374,10 +401,11 @@ class SignalManager:
             self.correct_event_times(offsets,new_log_out)
 
     def set_wd(self,channels=None,meanCalcChans=None,meanApplyChans=None):
-        #Sets the working data to the selected channels (selects all channels by default)
-        #Optional: columns - list of channels to use
-        #          meanChans - The channels to calculate the mean from
-        #          meanApplyChans - The channels to apply the mean to (default = meanCalcChans)
+        """Sets the working data to the selected channels (selects all channels by default)
+        Keyword Arguments:
+        channels=None -- list of channels to use
+        meanChans=None -- The channels to calculate the mean from
+        meanApplyChans=None - The channels to apply the mean to (default = meanCalcChans)"""
     
         logger.info( "Loading working data")
         self.__wd = self.data(channels=channels if channels else self.channels())
@@ -387,19 +415,21 @@ class SignalManager:
             self.set_mean(meanCalcChans, meanApplyChans)
     
     def wc(self):
-        #Returns a list of channel names for the working data
+        """Returns a list of channel names for the working data"""
         return list(self.__wc)
     
     def set_fs(self,fs):
-        #Set the frequency that the data was sampled at
+        """Set the frequency that the data was sampled at
+        Keyword Arguments:
+        fs -- sample frequency"""
         self.__signals['fs']=fs
         self.__flushSignals__() 
 
     def splice(self,data=None,times=None,indices=None):
-        #Returns the signal specified between two time points
-        #Optional: data - the data to splice (default is the whole data set)
-        #          times - the start and end times to splice between
-        #          indices - the start and end indices
+        """Returns the signal specified between two time points
+        data=None -- The data to splice (default is the whole data set)
+        times=None -- The start and end times to splice between
+        indices=None -- The start and end indices"""
         
         if data is None:
             data = self.wd()
@@ -410,9 +440,9 @@ class SignalManager:
             return data.iloc[indices]
     
     def eventsTimes(self,events,limit=None):
-        #Get combined time indices of each event period
-        #events - events to index
-        #limit - specify cut-off for each event in seconds
+        """Get combined time indices of each event period
+        events - Events to index
+        limit=None - Specify cut-off for each event in seconds"""
         
         limit = None if limit is None else int(limit*self.fs())
         allTimes = np.array([])
@@ -423,20 +453,32 @@ class SignalManager:
     
     #Timing functions
     def snap_time(self,t):
-        #Finds the nearest time index to time t
+        """Finds the nearest time
+        Keyword Arguments:
+        t - Time to snap to
+        """
+        
         return self.time_to_index(t)/float(self.fs())
         #return self.times()[self.time_to_index(t)]
 
     def index_to_time(self,ix):
-        #Returns the time of a given index
+        """Returns the time of a given index
+        Keyword Arguments:
+        ix - Index of time"""
         return self.times().iloc[ix]
 
     def time_to_index(self,t):
-        #Returns the index of a given time point
+        """Returns the index of a given time point
+        Keyword Arguments:
+        t -- Time point"""
         return int(np.floor(t*float(self.fs())))
     
     def event_times(self,event=None,times=None):
-        #Returns a full list of event sample times
+        """Returns a full list of event sample times
+        Keyword Arguments:
+        event=None -- Event to retrieve time points of
+        times=None -- Start and stop times to find times between
+        """
         if event is not None:
             [start,stop] = event[['pulse.on','pulse.off']].values
         elif times is not None:
@@ -449,12 +491,18 @@ class SignalManager:
         return self.times()[self.time_to_index(start):self.time_to_index(stop)]
     
     def event_data(self,event,chans=None):
-        #Returns the data for a given event
+        """Returns the data for a given event
+        Keyword Arguments:
+        event -- Event to get data from
+        chans=None -- Channels to get from"""
         if chans is None:
             chans = self.wc()
     
     def num_points(self,event=None,times=None):
-        #Returns the number of (inclusive) samples between two data points
+        """Returns the number of (inclusive) samples between two data points
+        Keyword Arguments:
+        event -- Event to get number of points of
+        times -- start and stop time to get number of points between"""
         #If fs is specified then use that, otherwise will need to snip a section and check the length
         if event is not None:
             return self.time_to_index(event['pulse.off'])-self.time_to_index(event['pulse.on'])
